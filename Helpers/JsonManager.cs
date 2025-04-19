a899d21e00a716c2b7f71bce0b4888592d189a78
Revv <revvn.01@gmail.com>
2025-04-18 23:55:42 -0600

    Se agrego la clase JsonManager en la carpeta helpers

---
+++ Helpers/JsonManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Windows.Navigation;
+
+namespace PlanSwiftApi.Helpers
+{
+    internal class JsonManager
+    {
+
+        public string GetConfigs()
+        {
+            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config", "Config.json");
+
+            if (!File.Exists(jsonPath)) 
+            {
+                
+                return null;
+            }
+            else
+            {
+                string json = File.ReadAllText(jsonPath);
+
+                var config = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>> (json);
+
+                if (config != null &&
+                    config.TryGetValue("Configs", out var nested) &&
+                    nested.TryGetValue("Path", out var path))
+                {
+                    return path;
+                }
+            }
+            return Constants.DefaultPath;
+        }
+
+    }
+}
